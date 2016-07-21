61b571c034a6b60eb37480060176b6f328f42b90
Daniel Lemos <xspager@gmail.com>
2016-07-21 15:01:06 -0300

    Create partition and run mkdosfs with sudo

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-sudo losetup --offset 1048576 --sizelimit 46934528 /dev/loop0 uefi.img
+if [ ! -f uefi.img ]; then
+	dd if=/dev/zero of=uefi.img bs=512 count=93750
+	sgdisk -o uefi.img 
+	sgdisk -n 0 uefi.img 
+	sgdisk -t 0:ef00 uefi.img 
+	sudo losetup --offset 1048576 --sizelimit 46934528 /dev/loop0 uefi.img
+	sudo mkdosfs -F 32 /dev/loop0
+else
+	sudo losetup --offset 1048576 --sizelimit 46934528 /dev/loop0 uefi.img
+fi
+
 sudo mount /dev/loop0 /mnt
 #sudo mkdir -p /mnt/EFI/BOOT
 #sudo cp main.efi /mnt/EFI/BOOT
